ab33c2178e7f0ddf6bfe894b61144cd733916703
Kyle Dixler <kdixle2@gmail.com>
2022-06-18 07:07:02 +0000

    refactor

--- rplugin/python3/github.py
+++ rplugin/python3/github.py
@@ -3,55 +3,110 @@ import os
 import base64
 import pynvim
 
+class GitPermalink:
+    @staticmethod
+    def from_permalink(permalink: str):
+        remote, rest = permalink.split('/tree/', maxsplit=1)
+        commit, rest = rest.split('/', maxsplit=1)
 
-def get_git_commentlink(fullpath: str, lines, notable_ref='origin/master', memo={}) -> str:
-    d = os.path.dirname(fullpath)
-    start = lines[0]
-    end = lines[1]
+        if '#' not in rest:
+            path = rest
+            return GitPermalink(remote, path, commit)
 
-    with os.popen(f"git -C '{d}' remote get-url origin") as p:
-        base_url = p.read().strip()
-        if 'github.com' not in base_url:
-            raise Exception("Not a GitHub repository")
+        path, rest = rest.split('#', maxsplit=1)
 
-    with os.popen(f"git log -1 --format=format:%H {fullpath}") as p:
-        commit = p.read().strip()
+        if not rest.startswith('L'):
+            return GitPermalink(remote, path, commit)
 
-    with os.popen(f"git -C '{d}' rev-parse --show-toplevel") as p:
-        root_path = p.read().strip() + '/'
-        path = fullpath[len(root_path):]
-    permalink = f"{base_url}/tree/{commit}/{path}"
+        rest = rest.replace('L', '')
+        start = None
+        if '-' not in rest:
+            start = int(rest)
+            return GitPermalink(remote, path, commit, start=start)
 
-    if not lines:
+        start, end = rest.split('-')
+        start, end = int(start), int(end)
+
+        return GitPermalink(remote, path, commit, start=start, end=end)
+
+    def __init__(self, remote: str, path: str, commit: str, start=None, end=None):
+        self.remote = remote
+        self.path = path
+        self.commit = commit
+        self.start = start
+        self.end = end
+
+    def serialize(self):
+        permalink = f"{self.remote}/tree/{self.commit}/{self.path}"
+
+        if not self.start:
+            return permalink
+
+        permalink += f"#L{self.start}"
+        if not self.end or self.start == self.end:
+            return permalink
+
+        permalink += f"-L{self.end}"
         return permalink
 
-    if lines[0] == lines[1]:
-        return f"{permalink}#L{lines[0]}"
-    return f"{permalink}#L{lines[0]}-L{lines[1]}"
+class GitRepo:
+    def __init__(self, root):
+        self.root = os.path.abspath(root)
 
-def get_git_permalink(fullpath: str, lines=None) -> str:
-    d = os.path.dirname(fullpath)
+    def get_remote(self, remote='origin'):
+        return self._git(f"remote get-url {remote}")
 
-    with os.popen(f"git -C '{d}' remote get-url origin") as p:
-        base_url = p.read().strip()
-        if 'github.com' not in base_url:
-            raise Exception("Not a GitHub repository")
-    #structure = "https://github.com/<org-name>/<repository>/tree/<commit-sha1>/<path/path/path>"
+    def get_file_last_commit(self, path):
+        return self._git(f"log -1 --format=format:%H {path}")
 
-    with os.popen(f"git -C '{d}' rev-parse HEAD") as p:
-        commit = p.read().strip()
+    def get_head_commit(self):
+        return self._git(f"rev-parse HEAD")
 
-    with os.popen(f"git -C '{d}' rev-parse --show-toplevel") as p:
-        root_path = p.read().strip() + '/'
-        path = fullpath[len(root_path):]
-    permalink = f"{base_url}/tree/{commit}/{path}"
+    def to_relative_path(self, abs_path):
+        abs_path = os.path.abspath(abs_path)
+        return abs_path[len(self.root + '/'):]
 
-    if not lines:
-        return permalink
+    def is_dirty(self, path):
+        out = self._git(f'diff {path}')
+        return out != ''
 
-    if lines[0] == lines[1]:
-        return f"{permalink}#L{lines[0]}"
-    return f"{permalink}#L{lines[0]}-L{lines[1]}"
+    def _git(self, args):
+        with os.popen(f"git -C '{self.root}' {args}") as p:
+            return p.read().strip()
+
+    @staticmethod
+    def from_path(path='.'):
+        d = os.path.dirname(path)
+        with os.popen(f"git -C '{d}' rev-parse --show-toplevel") as p:
+            root = p.read().strip()
+            return GitRepo(root)
+        
+
+def get_git_commentlink(fullpath: str, start, end) -> str:
+    repo = GitRepo.from_path(fullpath)
+
+    remote = repo.get_remote()
+    if 'github.com' not in remote:
+        raise Exception("Not a GitHub repository")
+
+    commit = repo.get_file_last_commit(fullpath)
+
+    path = repo.to_relative_path(fullpath)
+
+    return GitPermalink(remote, path, commit, start=start, end=end).serialize()
+
+def get_git_permalink(fullpath: str, start=None, end=None) -> str:
+    repo = GitRepo.from_path(fullpath)
+
+    remote = repo.get_remote()
+    if 'github.com' not in remote:
+        raise Exception("Not a GitHub repository")
+
+    commit = repo.get_head_commit()
+
+    path = repo.to_relative_path(fullpath)
+
+    return GitPermalink(remote, path, commit, start=start, end=end).serialize()
 
 class HLManager:
     def __init__(self, nvim):
@@ -73,6 +128,7 @@ class HLManager:
             for lineno in self.marks[bnr].keys():
                 self.nvim.command(f"sign unplace {lineno}")
         self.marks = {}
+        
 
 @pynvim.plugin
 class TestPlugin(object):
@@ -87,7 +143,7 @@ class TestPlugin(object):
         bnr = self.nvim.current.buffer
         start = self.nvim.api.buf_get_mark(bnr, "<")
         end = self.nvim.api.buf_get_mark(bnr, ">")
-        permalink = get_git_permalink(fullpath, lines=[start[0], end[0]])
+        permalink = get_git_permalink(fullpath, start=start[0], end=end[0])
         self.nvim.feedkeys(f'gv')
         self.nvim.command(f'echom "{permalink}"')
 
@@ -95,17 +151,23 @@ class TestPlugin(object):
     def comment(self, *args, **kwargs):
         fullpath = self.nvim.funcs.expand('%:p')
         bnr = self.nvim.current.buffer
+
+        repo = GitRepo.from_path(fullpath)
+        # check if file is dirty
+        if repo.is_dirty(fullpath):
+            self.nvim.command(f'echom "unable to add comments to dirty file(yet)"')
+            return
+
         start = self.nvim.api.buf_get_mark(bnr, "<")[0]
         end = self.nvim.api.buf_get_mark(bnr, ">")[0]
-        permalink = get_git_commentlink(fullpath, lines=[start, end])
+        permalink = get_git_commentlink(fullpath, start, end)
         self.nvim.command(f'echom "{permalink}"')
         self.highlight_manager.mark_lines(start, end)
         # TODO open buffer to read(or create if not exists) comment
 
 
-        d = os.path.dirname(fullpath)
-        with os.popen(f"git -C '{d}' rev-parse --show-toplevel") as p:
-            root_path = p.read().strip() + '/.git/gossip'
+        repo = GitRepo.from_path(fullpath)
+        root_path = os.path.join(repo.root, '.git', 'gossip')
 
         try: os.mkdir(root_path)
         except FileExistsError:...
@@ -118,4 +180,19 @@ class TestPlugin(object):
 
     @pynvim.autocmd("BufDelete", pattern="*.gossip.md")
     def clear_comment_hl(self, *args, **kwargs):
+        self.highlight_manager.unmark()
+
+    def get_comments(self, fullpath):
+        with os.popen('git log --format=format:%H init.vim'):
+            return []
+
+    @pynvim.command("CommentsOn", nargs="*", range="", sync=False)
+    def show_comments(self, *args, **kwargs):
+        fullpath = self.nvim.funcs.expand('%:p')
+        comments = self.get_comments(fullpath)
+        for comment in comments:
+            self.highlight_manager.mark_lines(comment.start, comment.end)
+
+    @pynvim.command("CommentsOff", nargs="*", range="", sync=False)
+    def hide_comments(self, *args, **kwargs):
         self.highlight_manager.unmark()
